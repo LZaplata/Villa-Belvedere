ce50a49cb98c1713969452b4ff5e55c8702814c5
zap <zap@hucr.cz>
2022-02-04 10:48:18 +0100

    theme initialization

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2,4 +2,14 @@ $(document).ready(function () {
     $(".offcanvas .nav li a").click(function () {
         $(".offcanvas button").click();
     });
+
+    var apartmentsCollapse = document.getElementById("more-apartments");
+
+    apartmentsCollapse.addEventListener("shown.bs.collapse", function () {
+        $("#cenik a[href='#more-apartments'] span").toggleClass("d-none");
+    });
+
+    apartmentsCollapse.addEventListener("hidden.bs.collapse", function () {
+        $("#cenik a[href='#more-apartments'] span").toggleClass("d-none");
+    });
 });
